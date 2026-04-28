0567d8248c03446f88d04bb1017d0b1bdd181887
kristinsainani <132377544+kristinsainani@users.noreply.github.com>
2026-04-28 13:03:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ if st.button("🎲 Run Study", use_container_width=True):
     final_reel = None
 
     for i in range(10):
-        rreel = np.random.choice(fruits, 3, replace=False)
+        reel = np.random.choice(fruits, 3, replace=False)
         slot.markdown(
             f"<h1 style='text-align:center'>{reel[0]} {reel[1]} {reel[2]}</h1>",
             unsafe_allow_html=True
